fef5e98684579d11634b278505616593a9b644ba
adamzw33 <azahwee73@hotmail.com>
2024-11-07 16:20:00 -0700

    adam, removed useless file

--- src/pigment.c
+++ src/pigment.c
@@ -9,3 +9,12 @@ int loadPigmentData(char* filename, pigment_t* pArray, int* n) {
     }
 int count = 0;
 
+int loadPaintData(char* filename, paint_t* pArray, int* n){
+    in = fopen(*filename, "r");
+    get[500] = "";
+    // paints.dat: C.I.NAME, MARKETING NAME, Manufacturer, Code, Tr, St, VR, Gr, Bl, Df, HA, HS, Lf
+    while(!feof(in)){
+        *n = 0;
+        
+    }
+}
